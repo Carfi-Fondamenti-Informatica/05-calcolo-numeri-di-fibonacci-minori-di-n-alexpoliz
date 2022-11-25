6915a6eced9cb2cd0829aaaaf6080499f8eada5f
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-11-25 15:34:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
    int n=0, a=1, b=1, c=0;
    cin >> n;
    if (n>=2) {
-      cout << a << endl << b endl;
+      cout << a << endl << b << endl;
       for (int i=2; i < n; i++) {
          c=a+b;
          b=a;
