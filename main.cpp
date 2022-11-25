edf73dc64b61d017e98eaff6b5237c21b66de90c
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-11-25 15:33:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   int n=0, a=1, b=1, c=0;
+   cin >> n;
+   if (n>=2) {
+      cout << a << endl << b endl;
+      for (int i=2; i < n; i++) {
+         c=a+b;
+         b=a;
+         b=c:
+         cout << c << endl;
+      }
+   } else { cout << "errore" << endl;
+          }
+    
    return 0;
 }
